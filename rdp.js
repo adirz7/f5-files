b8cda5a7e76a7e21f858af1b4ff929e71c483416
adirz7 <adir.ze@gmail.com>
2021-08-11 00:57:02 +0300

    Update rdp.js

--- rdp.js
+++ rdp.js
@@ -1,6 +1,15 @@
 <script>
-	document.getElementById('remote_desktop:\/Common\/RDP')
-			.addEventListener('click', function (event) {
-				alert("Please wait for the RDP Session to start!");
-			});
+	document.getElementById("remote_desktop:\/Common\/RDP").addEventListener("touchstart", touchHandler, false);
+	document.getElementById("remote_desktop:\/Common\/RDP").addEventListener("touchmove", touchHandler, false);
+	document.getElementById("remote_desktop:\/Common\/RDP").addEventListener("touchend", touchHandler, false);
+
+	function touchHandler(e) {
+	  if (e.type == "touchstart") {
+		alert("You touched the screen!");
+	  } else if (e.type == "touchmove") {
+		alert("You moved your finger!");
+	  } else if (e.type == "touchend" || e.type == "touchcancel") {
+		alert("You removed your finger from the screen!");
+	  }
+	}
 </script>
