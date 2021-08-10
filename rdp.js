9b7602223c8ba47d6982a14bff485a721a3b6c48
adirz7 <adir.ze@gmail.com>
2021-08-11 01:40:56 +0300

    Update rdp.js

--- rdp.js
+++ rdp.js
@@ -1,13 +1,4 @@
-	document.getElementById("remote_desktop:\/Common\/RDP").addEventListener("touchstart", touchHandler, false);
-	document.getElementById("remote_desktop:\/Common\/RDP").addEventListener("touchmove", touchHandler, false);
-	document.getElementById("remote_desktop:\/Common\/RDP").addEventListener("touchend", touchHandler, false);
-
-	function touchHandler(e) {
-	  if (e.type == "touchstart") {
-		alert("You touched the screen!");
-	  } else if (e.type == "touchmove") {
-		alert("You moved your finger!");
-	  } else if (e.type == "touchend" || e.type == "touchcancel") {
-		alert("You removed your finger from the screen!");
-	  }
-	}
+document.getElementById("remote_desktop:/Common/RDP")
+        .addEventListener('click', function (event) {
+             alert('Please standby for the RDP Session to start!');
+        });
