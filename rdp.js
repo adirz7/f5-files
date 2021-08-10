8a2a133b88b1c3278a3c21b4971b8bd783cacce0
adirz7 <adir.ze@gmail.com>
2021-08-11 00:14:09 +0300

    rdp.js
    test

---
+++ rdp.js
@@ -0,0 +1,4 @@
+document.getElementById('remote_desktop:/Common/RDP')
+        .addEventListener('click', function (event) {
+            alert("Please wait for the RDP Session to start!");
+        });
